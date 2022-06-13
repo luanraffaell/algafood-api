969fccfb15662fa0672ca35e7308f9b2ac6fb17c
Luan Rafael <luanraffaell@hotmail.com>
2022-06-13 09:45:49 -0300

    3.7. Importando dados de teste com import

--- algafood-api/src/main/java/com/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algafood/domain/model/Restaurante.java
@@ -5,11 +5,16 @@ import java.math.BigDecimal;
 import java.util.Objects;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Restaurante implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private BigDecimal taxaFrete;
