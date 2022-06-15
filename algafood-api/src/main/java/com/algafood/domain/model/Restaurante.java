6eaee6557c35b2e14a63519728bb73fbbc9a9dfc
Luan Rafael <luanraffaell@hotmail.com>
2022-06-15 11:26:53 -0300

    3.17. Mapeando relacionamento com @ManyToOne

--- algafood-api/src/main/java/com/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algafood/domain/model/Restaurante.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -23,6 +24,9 @@ public class Restaurante implements Serializable {
 	private Long id;
 	private String nome;
 	private BigDecimal taxaFrete;
+	
+	@ManyToOne
+	private Cozinha cozinha;
 	
 	
 }
