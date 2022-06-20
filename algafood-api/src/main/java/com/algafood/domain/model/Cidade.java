3731d13a5401e8ab921a0f702bb5384c9a46b581
Luan Rafael <luanraffaell@hotmail.com>
2022-06-20 12:06:51 -0300

    3.20. Desafio mapeando entidades

---
+++ algafood-api/src/main/java/com/algafood/domain/model/Cidade.java
@@ -0,0 +1,31 @@
+package com.algafood.domain.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Cidade implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	
+	@ManyToOne
+	@JoinColumn(name = "estado_id", nullable = false)
+	private Estado estado;
+
+}
