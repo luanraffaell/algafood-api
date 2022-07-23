ec285e716e7d2314f28a388140850e3a2000a6bb
Luan Rafael <luanraffaell@hotmail.com>
2022-07-23 13:18:35 -0300

    5.10. Externalizando consultas JPQL para um arquivo XML2

--- algafood-api/src/main/java/com/algafood/domain/repository/RestauranteRepository.java
+++ algafood-api/src/main/java/com/algafood/domain/repository/RestauranteRepository.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.algafood.domain.model.Restaurante;
