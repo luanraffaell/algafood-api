4db1644d89504b73063d25dfcddf7133728913f9
Luan Rafael <luanraffaell@hotmail.com>
2022-07-26 21:39:54 -0300

    7.3. Configurando o pool de conexões do Hikari

--- algafood-api/src/main/java/com/algafood/domain/repository/RestauranteRepository.java
+++ algafood-api/src/main/java/com/algafood/domain/repository/RestauranteRepository.java
@@ -5,12 +5,15 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.algafood.domain.model.Restaurante;
 
 public interface RestauranteRepository extends JpaRepository<Restaurante, Long>, RestauranteRepositoryQueries, JpaSpecificationExecutor<Restaurante> {
 	
+	@Query("from Restaurante r join r.cozinha")
+	List<Restaurante>findAll();
 	//@Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
 	List<Restaurante> consultaPorNome(String nome, @Param("id") Long cozinha);
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial,BigDecimal taxaFinal);
